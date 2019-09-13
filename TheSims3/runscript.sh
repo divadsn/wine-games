fb8d0f2ab3973cf9fbe6eecb7a02f13e27b9a097
David Sn <divad.nnamtdeis@gmail.com>
2019-09-13 03:42:49 +0200

    TheSims3: Fix desktop entry and enable FPS overlay
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- TheSims3/runscript.sh
+++ TheSims3/runscript.sh
@@ -4,4 +4,4 @@ export WINEPREFIX=~/.local/share/wineprefixes/TheSims3
 export WINEARCH=win32
 
 LAUNCHER=$(find $WINEPREFIX/drive_c -name "Sims3LauncherW.exe" -print -quit)
-wine "$LAUNCHER"
+DXVK_HUD=1 wine "$LAUNCHER"
