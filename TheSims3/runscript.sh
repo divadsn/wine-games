fe872625be53eee42d18f242296539354af1d1cd
David Sn <divad.nnamtdeis@gmail.com>
2019-09-16 21:15:27 +0200

    TheSims3: Make runscript to find any game executable as parameter
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- TheSims3/runscript.sh
+++ TheSims3/runscript.sh
@@ -3,9 +3,17 @@
 export WINEPREFIX=~/.local/share/wineprefixes/TheSims3
 export WINEARCH=win32
 
-cd $WINEPREFIX
-echo "Searching for Sims3LauncherW.exe inside wineprefix..."
-LAUNCHER=$(find ./drive_c -name "Sims3LauncherW.exe" -print -quit)
+if [ -z "$1" ]; then
+    echo "No file to run supplied! Usage: runscript.sh <executable_name>"
+    exit 1
+fi
 
-echo "Found: $LAUNCHER"
-DXVK_HUD=fps wine "$LAUNCHER"
+cd $WINEPREFIX
+echo "Searching for $1 inside wineprefix..."
+
+LAUNCHER=$(find ./drive_c -name $1 -print -quit)
+BASEDIR=$(dirname "$LAUNCHER")
+echo "Found: $BASEDIR"
+
+cd "$BASEDIR"
+DXVK_HUD=fps wine $1
